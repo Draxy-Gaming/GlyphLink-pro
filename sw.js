e643d4430bd76084c03f79dcb333ad2b1b533f8c
MD IFTEKHAR FERDOUS <140737843+Draxy-Gaming@users.noreply.github.com>
2026-01-12 19:09:23 +0600

    update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,15 @@
-const CACHE_NAME = 'glyphlink-pro-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'glyphlink-pro-v4';
+const assets = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(assets)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
-    ))
-  );
+  e.waitUntil(caches.keys().then((k) => Promise.all(k.map((n) => n !== CACHE_NAME && caches.delete(n)))));
 });
